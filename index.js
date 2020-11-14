09e6122d0979929232b2abafeaf95868d7d08117
aljt <alvinljt@gmail.com>
2020-11-15 00:39:43 +0800

    implemented 3.1.3 key-values section

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const app = express();
 // set the library (template engine || view engine) to use for all requests
 app.set('view engine', 'ejs');
 
+// set configuration to receive POST request data as an object
+app.use(express.urlencoded({ extended: false }));
+
 const PORT = 3004;
 
 // request callback function to render the html data (html data is alr in ejs file)
@@ -20,6 +23,13 @@ const whenRequestForHtml = (request, response) => {
 
 // set the route for main page
 app.get('/', whenRequestForHtml);
+
+// Routes HTTP POST requests to the specified path with the specified callback functions
+// documentation: https://expressjs.com/en/api.html#express.urlencoded
+app.post('/recipe', (request, response) => {
+  console.log('request body:', request.body);
+  response.send('we saved your recipe');
+});
 
 // initialise the request listener port functionality
 app.listen(PORT);
