1ae83d9da74b62eebaaa1df62bbe0aea3d69647d
aljt <alvinljt@gmail.com>
2020-11-15 01:24:26 +0800

    implemented 3.1.3 post+json section

--- index.js
+++ index.js
@@ -1,35 +1,74 @@
 import express from 'express';
 
+import { add } from './jsonFileStorage.js';
+
 const app = express();
-
-// set the library (template engine || view engine) to use for all requests
-app.set('view engine', 'ejs');
-
-// set configuration to receive POST request data as an object
-app.use(express.urlencoded({ extended: false }));
 
 const PORT = 3004;
 
-// request callback function to render the html data (html data is alr in ejs file)
-const whenRequestForHtml = (request, response) => {
-  console.log('request came in');
+// set the view engine to ejs
+app.set('view engine', 'ejs');
 
-  // render the html data in html.ejs file
-  response.render('html');
+// config to accept request form data
+app.use(express.urlencoded({ extended: false }));
 
-  // for render documentation:
-  // https://expressjs.com/en/api.html#res.render
-};
-
-// set the route for main page
-app.get('/', whenRequestForHtml);
-
-// Routes HTTP POST requests to the specified path with the specified callback functions
-// documentation: https://expressjs.com/en/api.html#express.urlencoded
+// app.post - accept form request
 app.post('/recipe', (request, response) => {
-  console.log('request body:', request.body);
-  response.send('we saved your recipe');
+  console.log('request body', request.body);
+
+  // add an element to the array
+  add('data.json', 'recipes', request.body, (data, error) => {
+    // check for errors
+    if (error) {
+      response.status(500).send('sorry this didnt work');
+      return;
+    }
+
+    // send back an acknowledgement
+    response.send('done!');
+  });
 });
 
-// initialise the request listener port functionality
+// render the form that will create the request
+app.get('/recipe', (request, response) => {
+  response.render('recipe');
+});
+
 app.listen(PORT);
+
+// This section is for key-value section =======================================================
+// import express from 'express';
+
+// const app = express();
+
+// // set the library (template engine || view engine) to use for all requests
+// app.set('view engine', 'ejs');
+
+// // set configuration to receive POST request data as an object
+// app.use(express.urlencoded({ extended: false }));
+
+// const PORT = 3004;
+
+// // request callback function to render the html data (html data is alr in ejs file)
+// const whenRequestForHtml = (request, response) => {
+//   console.log('request came in');
+
+//   // render the html data in html.ejs file
+//   response.render('html');
+
+//   // for render documentation:
+//   // https://expressjs.com/en/api.html#res.render
+// };
+
+// // set the route for main page
+// app.get('/', whenRequestForHtml);
+
+// // Routes HTTP POST requests to the specified path with the specified callback functions
+// // documentation: https://expressjs.com/en/api.html#express.urlencoded
+// app.post('/recipe', (request, response) => {
+//   console.log('request body:', request.body);
+//   response.send('we saved your recipe');
+// });
+
+// // initialise the request listener port functionality
+// app.listen(PORT);
