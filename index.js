0ae0cdd9773fcc879a36edcfe60d9d1b44e92a23
aljt <alvinljt@gmail.com>
2020-11-14 23:42:05 +0800

    implemented 3.1.2 form request section

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log('hello world');
+import express from 'express';
+
+const app = express();
+
+// set the library (template engine || view engine) to use for all requests
+app.set('view engine', 'ejs');
+
+const PORT = 3004;
+
+// request callback function to render the html data (html data is alr in ejs file)
+const whenRequestForHtml = (request, response) => {
+  console.log('request came in');
+
+  // render the html data in html.ejs file
+  response.render('html');
+
+  // for render documentation:
+  // https://expressjs.com/en/api.html#res.render
+};
+
+// set the route for main page
+app.get('/', whenRequestForHtml);
+
+// initialise the request listener port functionality
+app.listen(PORT);
